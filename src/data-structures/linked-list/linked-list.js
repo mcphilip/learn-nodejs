585d3dea324b2c89e0cbae1b0b9483291549ec15
Philip McClelland <pmcclelland@arganteal.com>
2019-01-21 20:44:31 -0600

    adding findNode, insertAfter, and removeFirst.  adding tests

--- src/data-structures/linked-list/linked-list.js
+++ src/data-structures/linked-list/linked-list.js
@@ -14,6 +14,44 @@ module.exports = class LinkedList {
             this.head = newHead;
         }
     }
+    append(data) {
+        const newNode = new Node(data);
+        if( !this.head ) {
+            this.head = newNode;
+        }
+        else {
+            let endNode = this.head;
+            while(endNode.next) {
+                endNode = endNode.next;
+            }
+            endNode.next = newNode;
+        }
+    }
+    insertAfter(targetData, data) {
+        let target = this.findNode(targetData);
+        if( target ) {
+            const newNode = new Node(data);
+            if( target.next ) {
+                newNode.next = target.next;
+            }
+            target.next = newNode;
+        }
+    }
+    findNode(data) {
+        let node = this.head;
+        while(node) {
+            if(node.data === data) {
+                return node;
+            }
+            node = node.next;
+        }
+        return null;
+    }
+    removeFirst() {
+        if(this.head) {
+            this.head = this.head.next ? this.head.next : null;
+        }
+    }
     toString() {
         let current = this.head;
         let display = '';
--- test/data-structures/linked-list/linked-list-tests.js
+++ test/data-structures/linked-list/linked-list-tests.js
@@ -11,4 +11,71 @@ describe('LinkedList', function() {
             expect(list.toString()).to.equal('2 -> 1');
         });
     });
+    describe('append', function() {
+        it('should add data to end of linked list', function() {
+            const list = new LinkedList();
+            list.prepend(1);
+            list.append(2);
+            expect(list.toString()).to.equal('1 -> 2');
+        });
+        it('should add data to end of empty list', function() {
+            const list = new LinkedList();
+            list.append(1);
+            expect(list.toString()).to.equal('1');
+        });
+    });
+    describe('findNode', function() {
+        it('should return node with matching data', function() {
+            const list = new LinkedList();
+            list.prepend(1);
+            list.prepend(2);
+            const foundNode = list.findNode(1);
+            expect(foundNode.data).to.equal(1);
+        });
+        it('should return null when no match found', function() {
+            const list = new LinkedList();
+            list.append(1);
+            expect(list.findNode(2)).to.equal(null);
+        });
+        it('should return null on empty list', function() {
+            const list = new LinkedList();
+            expect(list.findNode(1)).to.equal(null);
+        });
+    });
+    describe('insertAfter', function() {
+        it('should not change list if value not found', function() {
+            const list = new LinkedList();
+            list.append(1);
+            list.append(2);
+            list.insertAfter(3, 1);
+            expect(list.toString()).to.equal('1 -> 2');
+        });
+        it('should do nothing to an empty list', function() {
+            const list = new LinkedList();
+            list.insertAfter(1, 2);
+            expect(list.toString()).to.equal('');
+        });
+        it('should insert after first occurence of matching data', function() {
+            const list = new LinkedList();
+            list.append(1);
+            list.append(2);
+            list.append(1);
+            list.insertAfter(1,3);
+            expect(list.toString()).to.equal('1 -> 3 -> 2 -> 1');
+        });
+    });
+    describe('removeFirst', function() {
+        it('should do nothing to empty list', function() {
+            const list = new LinkedList();
+            list.removeFirst();
+            expect(list.toString()).to.equal('');
+        });
+        it('should remove first node', function() {
+            const list = new LinkedList();
+            list.append(1);
+            list.append(2);
+            list.removeFirst();
+            expect(list.toString()).to.equal('2');
+        });
+    });
 });
